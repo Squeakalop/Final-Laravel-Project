44cac8a00451467ab48c726b4ce3883045192048
Jesse <w0291240@nscc.ca>
2025-12-05 13:58:37 -0400

    initial commit

--- 2025-laravel-starter/routes/web.php
+++ 2025-laravel-starter/routes/web.php
@@ -9,3 +9,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('categories', App\Http\Controllers\CategoryController::class);
+Route::resource('items', App\Http\Controllers\ItemController::class);
